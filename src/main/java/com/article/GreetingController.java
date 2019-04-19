9ee49fc684745156a4f7c5a2092274be9be46f44
Mehmet Erdogan <merdogan67@gmail.com>
2019-04-19 16:25:48 +0800

    JIRA-001: Init eureka-client project

---
+++ src/main/java/com/article/GreetingController.java
@@ -0,0 +1,9 @@
+package com.article;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+
+    @RequestMapping("/greeting")
+    String greeting();
+}
